dc93387ee6d040b5c7786931091c7317f115aace
Marc Horne <me@marcushorne.com>
2017-01-29 19:31:57 +0100

    Remove modules/feeds.js from gitignore

---
+++ modules/feeds.js
@@ -0,0 +1,9 @@
+module.exports = [
+  'http://feeds.feedburner.com/CssTricks',
+  'http://feeds.feedburner.com/html5rocks',
+  'http://feeds.feedburner.com/alistapart/abridged',
+  'http://feeds.feedburner.com/OpenWebPlatformDailyDigest',
+  'http://www.smashingmagazine.com/feed',
+  'http://feeds.uxmag.com/uxm',
+  'https://feeds.feedblitz.com/scotch_io'
+];
